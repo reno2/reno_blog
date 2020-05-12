4076b89c55a46e1d020ed0b16f8d8065aa87a0ee
reno <chedia@mail.ru>
2020-05-12 16:08:54 +0300

    front_end_footer down

--- resources/views/chunks/check_admin.blade.php
+++ resources/views/chunks/check_admin.blade.php
@@ -1,5 +1,6 @@
 @if(Auth::user())
 	@if(Auth::user()->roles()->get()->pluck('name')->contains('admin'))
-        <a style="display: block; text-align: right;" class="article__edit pull-right" href="{{route('admin.article.edit', $article)}}">Изменить</a>
+        
+        <a style="display: block; text-align: right;" class="article__edit pull-right" href="{{route('admin.article.edit', $article)}}"><i class="fas fa-pencil-alt"></i></a>
     @endif	
 @endif	
