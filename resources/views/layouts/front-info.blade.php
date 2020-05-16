c7ba2ce77c3d3eb76e96e616be93cd30ea63bf0a
reno <chedia@mail.ru>
2020-05-16 14:18:10 +0300

    front_2

--- resources/views/layouts/front-info.blade.php
+++ resources/views/layouts/front-info.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
 
-            <div class="info__item s-block">
+            <div class="info__item s-block s-block__lime_topl">
                 <div class="s-block__icon"><img src="/images/service1.png" alt=""></div>
                 <div class="s-block__title">Разработка</div>
                 <div class="s-block__text">Разработка на популярныз CMS оптыт в реализованных задачах</div>
@@ -17,7 +17,7 @@
                 <div class="s-block__title">Разработка</div>
                 <div class="s-block__text">Разработка на популярныз CMS оптыт в реализованных задачах</div>
             </div>
-            <div class="info__item s-block">
+            <div class="info__item s-block s-block__lime_bottomr">
                 <div class="s-block__icon"><img src="/images/service4.png" alt=""></div>
                 <div class="s-block__title">Разработка</div>
                 <div class="s-block__text">Разработка на популярныз CMS оптыт в реализованных задачах</div>
